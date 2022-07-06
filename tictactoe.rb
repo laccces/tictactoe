503cfb43e8a8af9fa430eebf129f4503a131de0f
Dan <en07djc@icloud.com>
2022-07-06 09:21:10 +0100

    Restructuring code to incorporate methods for the boards and user moves.

--- tictactoe.rb
+++ tictactoe.rb
@@ -1,9 +1,9 @@
 # https://replit.com/@lacesoutdan/ProudTestyReality#main.rb 
 
 #reference
-blank = [["O", "O", "O"], ["O", "O", "O"], ["O", "O", "O"]]  
-divider = "--+---+--"
-
+@blank = [["O", "O", "O"], ["O", "O", "O"], ["O", "O", "O"]]  
+@divider = "--+---+--"
+@example_board = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 
 =begin
 puts "Enter name for Player 1:"
@@ -13,56 +13,60 @@ puts "Enter name for Player 2:"
 player_two = gets
 =end
 
-puts "GENERATED NEW BOARD:"
-puts "#{blank[0][0]} | #{blank[0][1]} | #{blank[0][2]}"
-  puts divider
-  puts "#{blank[1][0]} | #{blank[1][1]} | #{blank[1][2]}"
-  puts divider
-  puts "#{blank[2][0]} | #{blank[2][1]} | #{blank[2][2]}"
+def new_board
+  puts "#{@blank[0][0]} | #{@blank[0][1]} | #{@blank[0][2]}"
+  puts @divider
+  puts "#{@blank[1][0]} | #{@blank[1][1]} | #{@blank[1][2]}"
+  puts @divider
+  puts "#{@blank[2][0]} | #{@blank[2][1]} | #{@blank[2][2]}"
+end 
 
-def board_index()
+def board_index
   puts "Here's the board index:"
-  example_board = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-  divider = "--+---+--"
-  puts "#{example_board[0][0]} | #{example_board[0][1]} | #{example_board[0][2]}"
-  puts divider
-  puts "#{example_board[1][0]} | #{example_board[1][1]} | #{example_board[1][2]}"
-  puts divider
-  puts "#{example_board[2][0]} | #{example_board[2][1]} | #{example_board[2][2]}"
+  puts "#{@example_board[0][0]} | #{@example_board[0][1]} | #{@example_board[0][2]}"
+  puts @divider
+  puts "#{@example_board[1][0]} | #{@example_board[1][1]} | #{@example_board[1][2]}"
+  puts @divider
+  puts "#{@example_board[2][0]} | #{@example_board[2][1]} | #{@example_board[2][2]}"
 end
+
+puts "GENERATED NEW BOARD:"
+new_board
+
+board_index
 
 puts "Where would you like to go? Please enter a number between 1 & 9."
 
-user_choice = gets.to_i
+def user_move
+  user_choice = gets.to_i
 
-case user_choice
-  when 1
-    blank[0][0] = "X"
-  when 2
-    blank[0][1] = "X"
-  when 3
-    blank[0][2] = "X"
-  when 4
-    blank[1][0] = "X"
-  when 5
-    blank[1][1] = "X"
-  when 6
-    blank[1][2] = "X"
-  when 7
-    blank[2][0] = "X"
-  when 8
-    blank[2][1] = "X"
-  when 9
-    blank[2][2] = "X"
-  else 
-    puts "Error."
+  case user_choice
+    when 1
+      @blank[0][0] = "X"
+    when 2
+      @blank[0][1] = "X"
+    when 3
+      @blank[0][2] = "X"
+    when 4
+      @blank[1][0] = "X"
+    when 5
+      @blank[1][1] = "X"
+    when 6
+      @blank[1][2] = "X"
+    when 7
+      @blank[2][0] = "X"
+    when 8
+      @blank[2][1] = "X"
+    when 9
+      @blank[2][2] = "X"
+    else 
+      puts "Error."
+  end
 end
 
-puts "#{blank[0][0]} | #{blank[0][1]} | #{blank[0][2]}"
-puts divider
-puts "#{blank[1][0]} | #{blank[1][1]} | #{blank[1][2]}"
-puts divider
-puts "#{blank[2][0]} | #{blank[2][1]} | #{blank[2][2]}"
+user_move
+
+new_board
 
 #logic for wins/losses
 
