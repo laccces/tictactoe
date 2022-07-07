649394a1247f9dd790b4fe02759a623200778a39
Dan <en07djc@icloud.com>
2022-07-07 10:33:33 +0100

    Fixed check_winner if/else.

--- tictactoe.rb
+++ tictactoe.rb
@@ -5,15 +5,7 @@
 @divider = "--+---+--"
 @example_board = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 
-=begin
-puts "Enter name for Player 1:"
-player_one = gets
-
-puts "Enter name for Player 2:"
-player_two = gets
-=end
-
-def new_board
+def board
   puts "#{@blank[0][0]} | #{@blank[0][1]} | #{@blank[0][2]}"
   puts @divider
   puts "#{@blank[1][0]} | #{@blank[1][1]} | #{@blank[1][2]}"
@@ -29,13 +21,6 @@ def board_index
   puts @divider
   puts "#{@example_board[2][0]} | #{@example_board[2][1]} | #{@example_board[2][2]}"
 end
-
-puts "GENERATED NEW BOARD:"
-new_board
-
-board_index
-
-puts "Where would you like to go? Please enter a number between 1 & 9."
 
 def user_move
   user_choice = gets.to_i
@@ -64,29 +49,46 @@ def user_move
   end
 end
 
+def check_winner
+  if @blank[0][0] == "X" && @blank[0][1] == "X" && @blank[0][2] == "X"
+    return win
+  elsif @blank[1][0] == "X" && @blank[1][1] == "X" && @blank[1][2] == "X"
+    return win
+  elsif @blank[2][0] == "X" && @blank[2][1] == "X" && @blank[2][2] == "X"
+    return win
+  elsif @blank[0][0] == "X" && @blank[1][1] == "X" && @blank[2][2] == "X"
+    return win
+  elsif @blank[0][2] == "X" && @blank[1][1] == "X" && @blank[2][0] == "X"
+    return win
+  elsif @blank[0][0] == "X" && @blank[1][0] == "X" && @blank[2][0] == "X"
+    return win
+  elsif @blank[0][1] == "X" && @blank[1][1] == "X" &&  @blank[2][1] == "X"
+    return win
+  elsif @blank[0][2] == "X" && @blank[1][2] == "X" && @blank[2][2] == "X"
+    return win
+  end
+end
+
+#play the game
+
+=begin
+puts "Enter name for Player 1:"
+player_one = gets
+
+puts "Enter name for Player 2:"
+player_two = gets
+=end
+
+puts "GENERATED NEW BOARD:"
+board
+
+board_index
+
+puts "Where would you like to go? Please enter a number between 1 & 9."
+
 user_move
 
-new_board
+board
 
-#logic for wins/losses
+check_winner
 
-#horizontals
-if blank[0][0] == "X" && blank[0][1] == "X" && blank[0][2] == "X"
-
-if blank[1][0] = "X" && blank[1][1] = "X" && blank[1][2] = "X"
-
-if blank[2][0] = "X" && blank[2][1] = "X" && blank[2][2] = "X"
-
-#diagonals
-if blank[0][0] == "X" && blank[1][1] == "X" && blank[2][2] == "X"
-
-if blank[0][2] = "X" && blank[1][1] = "X" && blank[2][0] = "X"
-
-#verticals
-if blank[0][0] = "X" && blank[1][0] = "X" && blank[2][0] = "X"
-
-if blank[0][1] = "X" && blank[1][1] = "X" &&  blank[2][1] = "X"
-
-if blank[0][2] = "X" && blank[1][2] = "X" && blank[2][2] = "X"
-
-#this needs to happen after each move, and then puts #{player_who_made_last_move} wins!
